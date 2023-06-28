0b8e258f5e0f0027a1586e8e831f4b1085521f94
unknown <Adeleyeha.17@protonmail.com>
2023-06-28 13:41:23 +0100

    portfolio component updated state

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,8 +6,24 @@ import Quantum from "../../assets/fitness.png"
 import Eduford from "../../assets/university.jpeg"
 import Game from "../../assets/game.jpeg"
 import Playstation from "../../assets/playstation.jpeg"
+import Vid from "../../assets/solo.mp4"
 
 const Portfolio = () => {
+
+  const [videoVisible, setVideoVisible] = useState({
+    atlas: false,
+    quantum: false,
+    eduford: false,
+    game: false,
+    playstation: false
+  });
+
+  const toggleVideo = (image) => {
+    setVideoVisible((prevState) => ({
+      ...prevState,
+      [image]: !prevState[image]
+    }));
+  };
 
   const [toggle, setToggle] = useState(1)
 
@@ -36,15 +52,16 @@ const Portfolio = () => {
          <div className="portfolio__sections">
           <div className={toggle === 1 ? "portfolio__content portfolio__content-active" : "portfolio__content"}>
             <div className="portfolio__data ">
-            <div>
+            <div className="img__container" onMouseEnter={() => toggleVideo("atlas")} onMouseLeave={() => toggleVideo("atlas")}>
                 <img src={Atlas} alt="" className="img"/>
+                {videoVisible.atlas && <video src={Vid} autoPlay muted className="vid"></video>}
                 </div>
 
               <div>
                 <h3 className="portfolio__title"> ATLAS BANKING
                 </h3>
 
-                <p>lalalaalalla</p>
+                <p className="text">Atlas Banking is your trusted online bank, simplifying your financial journey with convenience and security. From managing your accounts to making seamless transactions, Atlas provide a user-friendly platform that puts user in control of their finances.</p>
                 <div className="gap">
                 <i className='bx bxl-html5 html'></i>                
                 <i className='bx bxl-css3 css'></i>
@@ -79,7 +96,7 @@ const Portfolio = () => {
                 <h3 className="portfolio__title"> QUANTUM FITNESS
                 </h3>
 
-                <p>lalalalalal</p>
+                <p className="text">Quantum Fitness is a fitness club, dedicated to helping you achieve your health and wellness goals. User can browse through QFitness state-of-the-art facilities, expert trainers, and a supportive community to keep you motivated and engaged through the webstite. Whether you are looking to lose weight, build muscle, or improve overall fitness, QFitness has the resources and expertise to guide you on your fitness journey. </p>
                 <div className="link">
                 <i className='bx bxl-react react'></i>                
                 <i className='bx bxl-css3 css'></i>
@@ -94,21 +111,25 @@ const Portfolio = () => {
               </div>
 
               
-
-              <img src={Quantum} alt="" className="img"/>
+                <div className="img__container" onMouseEnter={() => toggleVideo("quantum")} onMouseLeave={() => toggleVideo("quantum")}>
+                <img src={Quantum} alt="" className="img"/>
+              {videoVisible.quantum && <video src={Vid} autoPlay muted className="vid"></video>}
+                </div>
+              
 
             </div>
 
             <div className="portfolio__data ">
-            <div>
+            <div className="img__container" onMouseEnter={() => toggleVideo("eduford")} onMouseLeave={() => toggleVideo("eduford")}>
                 <img src={Eduford} alt="" className="img"/>
+                {videoVisible.eduford && <video src={Vid} autoPlay muted className="vid"></video>}
                 </div>
 
               <div>
                 <h3 className="portfolio__title"> EDUFORD
                 </h3>
 
-                <p>lalalaalalla</p>
+                <p className="text">Eduford website provides a comprehensive platform for students, faculty, and staff to access academic resources, connect with peers, and stay informed about campus events. From exploring degree programs to accessing course materials, Eduford is designed to streamline your educational journey.</p>
                 <div className="link">
                 <i className='bx bxl-react react'></i>                
                 <i className='bx bxl-css3 css'></i>
@@ -142,7 +163,7 @@ const Portfolio = () => {
                               <h3 className="portfolio__title"> GAME WEBSITE
                               </h3>
               
-                              <p>lalalalalal</p>
+                              <p className="text">Gamestore is your ultimate destination for all things gaming. Our website is a haven for gamers, offering a vast collection of the latest and greatest video games, consoles, and accessories. Browse through our extensive catalog, find exclusive deals, and stay updated on upcoming releases. Whether you are a casual gamer or a hardcore enthusiast, GameHub is your one-stop shop for immersive gaming experiences. </p>
                               <div className="gap">
                               <i className='bx bxl-html5 html'></i>                
                               <i className='bx bxl-css3 css'></i>
@@ -158,25 +179,29 @@ const Portfolio = () => {
                               
                             </div>
               
+                              <div className="img__container" onMouseEnter={() => toggleVideo("game")} onMouseLeave={() => toggleVideo("game")}>
+                              <img src={Game} alt="" className="img"/>
+                              {videoVisible.game && <video src={Vid} autoPlay muted className="vid"></video>}
+                              </div>
                             
-              
-                            <img src={Game} alt="" className="img"/>
               
                           </div>
               
 
-                          <div className="portfolio__data ">
-            <div>
+             <div className="portfolio__data ">
+             <div className="img__container" onMouseEnter={() => toggleVideo("playstation")} onMouseLeave={() => toggleVideo("playstation")}>
+
                 <img src={Playstation} alt="" className="img"/>
+                {videoVisible.playstation && <video src={Vid} autoPlay muted className="vid"></video>}
                 </div>
 
               <div>
                 <h3 className="portfolio__title"> PLAYSTATION WEBSITE
                 </h3>
 
-                <p>lalalaalalla</p>
-                <div className="link">
-                <i className='bx bxl-html html'></i>                
+                <p className="text">The PlayStation website is your gateway to a world of immersive gaming experiences. Stay updated on the latest PlayStation console releases, accessories, and exclusive features. Connect with a vibrant gaming community, join online multiplayer matches, and compete against players worldwide.</p>
+                <div className="gap">
+                <i className='bx bxl-html5 html'></i>                
                 <i className='bx bxl-css3 css'></i>
                 <i className='bx bxl-javascript javascript'></i>
                 </div>
